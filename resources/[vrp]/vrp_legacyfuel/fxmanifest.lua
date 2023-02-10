95c7414fe54b8db973e8fa1258795ee0d8e05537
Swellington Soares <swellington.santos@gmail.com>
2023-02-10 18:02:42 -0300

    resolução dos conflitos de arquivo

--- resources/[vrp]/vrp_legacyfuel/fxmanifest.lua
+++ resources/[vrp]/vrp_legacyfuel/fxmanifest.lua
@@ -1,14 +1,9 @@
-<<<<<<<< HEAD:resources/[vrp]/vrp_legacyfuel/fxmanifest.lua
 fx_version "cerulean"
 game "gta5"
 
 shared_scripts {
 	"@vrp/lib/utils.lua",
 }
-========
-fx_version 'cerulean'
-game 'gta5'
->>>>>>>> c9cbff15c7430ddd822b3202475b963fbba374de:resources/[vrp]/vrp_legacyfuel/fxmanifest.lua.lua
 
 server_scripts {
 	
