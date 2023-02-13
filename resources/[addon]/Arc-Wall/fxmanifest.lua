9d3429e470c00803067ca0769ca9efa86cb4e7dc
Alef <aleffariias@revorp.pc>
2023-02-13 20:32:33 +0000

    correção do wall

--- resources/[addon]/Arc-Wall/fxmanifest.lua
+++ resources/[addon]/Arc-Wall/fxmanifest.lua
@@ -1,27 +1,13 @@
-<<<<<<<< HEAD:resources/[addon]/Arc-Wall/fxmanifest.lua
-fx_version 'bodacious'
-game 'gta5'
-
-client_scripts {
-	"@vrp/lib/utils.lua",
-	"client.lua",
-	"config.lua"
-========
 fx_version "cerulean"
 game "gta5"
 
 client_scripts {
 	"@vrp/lib/utils.lua",
 	"client.lua"
->>>>>>>> 242a86fe6f86920f54636fcdcacf2d65f4737b28:resources/[vrp]/vrp_hospital/fxmanifest.lua
 }
 
 server_scripts {
 	"@vrp/lib/utils.lua",
-<<<<<<<< HEAD:resources/[addon]/Arc-Wall/fxmanifest.lua
 	"server.lua",
 	"config.lua"
-========
-	"server.lua"
->>>>>>>> 242a86fe6f86920f54636fcdcacf2d65f4737b28:resources/[vrp]/vrp_hospital/fxmanifest.lua
 }
